1b3405bf433334069beec75986c27fb8f66a89cf
Alabi Ayobami <arocket04@gmail.com>
2024-09-01 23:15:54 +0100

    changed primary swatch

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,8 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.purple,
         ),
         home: SummarizerScreen(),
       ),
