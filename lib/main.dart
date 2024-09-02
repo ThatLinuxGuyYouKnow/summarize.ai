e4757fd1a5abb54f6490d42143ddba820ac0d261
Alabi Ayobami <arocket04@gmail.com>
2024-09-02 23:35:53 +0100

    implemented cutom plash screen, no external package used

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:aisummarizer/State%20Management/summary_alert.dart';
+import 'package:aisummarizer/manualSplash.dart';
 import 'package:aisummarizer/summarizerScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.purple,
         ),
-        home: SummarizerScreen(),
+        home: Manualsplash(),
       ),
     );
   }
--- lib/manualSplash.dart
+++ lib/manualSplash.dart
@@ -1,8 +1,24 @@
+import 'dart:async';
+
+import 'package:aisummarizer/summarizerScreen.dart';
 import 'package:flutter/material.dart';
 
 class Manualsplash extends StatelessWidget {
   Manualsplash({super.key});
+  exitSplash(context) {
+    Timer(
+        Duration(seconds: 4),
+        () => Navigator.push<void>(
+              context,
+              MaterialPageRoute<void>(
+                builder: (BuildContext context) => SummarizerScreen(),
+              ),
+            ));
+  }
+
   Widget build(BuildContext context) {
+    exitSplash(context);
+
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     return SizedBox(
