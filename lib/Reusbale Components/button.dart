2a6f9ca36e50c6cdbfebf741416a790a2db70ece
Alabi Ayobami <arocket04@gmail.com>
2024-09-13 19:05:07 +0100

    made button width responsive

--- lib/Reusbale Components/button.dart
+++ lib/Reusbale Components/button.dart
@@ -8,6 +8,8 @@ class ReusableButton extends StatelessWidget {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
+
+    if (screenWidth >= 900) {}
     return ClipRRect(
         borderRadius: BorderRadius.circular(5),
         child: SizedBox(
