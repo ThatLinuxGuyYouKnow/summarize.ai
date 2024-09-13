c95c79363ef458cf72361c986a4b0527b084b0a8
Alabi Ayobami <arocket04@gmail.com>
2024-09-13 18:51:07 +0100

    width chained to screenWidth

--- lib/Reusbale Components/resuableTextField.dart
+++ lib/Reusbale Components/resuableTextField.dart
@@ -12,7 +12,8 @@ class ReusableTextField extends StatelessWidget {
   final Color? borderColor;
   final Function(String)? onChanged;
 
-  ReusableTextField({
+  const ReusableTextField({
+    Key? key,
     this.controller,
     this.hintText = '',
     this.keyboardType = TextInputType.text,
@@ -22,44 +23,44 @@ class ReusableTextField extends StatelessWidget {
     this.borderWidth,
     this.borderColor,
     this.onChanged,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-    double width = screenWidth * .9;
-    if (screenWidth >= 900) {}
+    double containerWidth = screenWidth * 0.9;
+
+    if (screenWidth >= 900) {
+      containerWidth = screenWidth * 0.3;
+    }
+
     return Container(
-      height: screenHeight * 0.08,
-      width: screenWidth * 0.9,
+      height: 50, // Fixed height for the input field
+      width: containerWidth, // Use the calculated width here
       padding: EdgeInsets.symmetric(horizontal: 10.0),
       decoration: BoxDecoration(
         border: Border.all(
-            width: borderWidth ?? 0, color: borderColor ?? Colors.transparent),
+          width: borderWidth ?? 1.0,
+          color: borderColor ?? Colors.grey,
+        ),
         color: Color.fromARGB(255, 245, 245, 245),
         borderRadius: BorderRadius.circular(10.0),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      child: Row(
         children: [
-          Row(
-            children: [
-              prefixIcon ?? SizedBox.shrink(),
-              Expanded(
-                child: TextField(
-                  controller: controller ?? null,
-                  keyboardType: keyboardType,
-                  obscureText: obscureText,
-                  onChanged: onChanged, // And this line
-                  decoration: InputDecoration(
-                    hintText: hintText,
-                    border: InputBorder.none,
-                    suffixIcon: suffixIcon,
-                  ),
-                ),
+          if (prefixIcon != null) prefixIcon!,
+          Expanded(
+            child: TextField(
+              controller: controller,
+              keyboardType: keyboardType,
+              obscureText: obscureText,
+              onChanged: onChanged,
+              decoration: InputDecoration(
+                hintText: hintText,
+                border: InputBorder.none,
+                suffixIcon: suffixIcon,
               ),
-            ],
+            ),
           ),
         ],
       ),
